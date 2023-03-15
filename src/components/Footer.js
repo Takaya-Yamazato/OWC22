8baa6d2268c1ec96503b8187982f7c8c76888efc
Takaya YAMAZATO <yamazato@ilas.nagoya-u.ac.jp>
2023-03-15 17:23:12 +0900

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
+import logo from "../img/logo.svg";
+import facebook from "../img/social/facebook.svg";
 // import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
+import twitter from "../img/social/twitter.svg";
 // import vimeo from '../img/social/vimeo.svg'
-import { FaWifi } from '@react-icons/all-files/fa/FaWifi'
+import { FaWifi } from "@react-icons/all-files/fa/FaWifi";
 
 const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
+          <img src={logo} alt="Kaldi" style={{ width: "14em", height: "10em" }} />
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
-            <div style={{ maxWidth: '100vw' }} className="columns">
+            <div style={{ maxWidth: "100vw" }} className="columns">
               <div className="column is-4">
                 <section className="menu">
                   <ul className="menu-list">
@@ -36,7 +32,7 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/Submission/">
+                      <Link className="navbar-item" to="/submission/">
                         Submission
                       </Link>
                     </li>
@@ -62,10 +58,7 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link
-                        className="navbar-item"
-                        to="https://icc2022.ieee-icc.org/"
-                      >
+                      <Link className="navbar-item" to="https://icc2022.ieee-icc.org/">
                         IEEE International Conference on Communications 2022
                       </Link>
                     </li>
@@ -74,29 +67,14 @@ const Footer = class extends React.Component {
               </div>
               <div className="column is-4 social">
                 <a title="facebook" href="https://www.facebook.com/IEEEICC/">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
+                  <img src={facebook} alt="Facebook" style={{ width: "1em", height: "1em" }} />
                 </a>
                 <a title="twitter" href="https://twitter.com/ieeeicc">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
+                  <img className="fas fa-lg" src={twitter} alt="Twitter" style={{ width: "1em", height: "1em" }} />
                 </a>
                 <br />
                 <br />
-                <a
-                  className="navbar-item"
-                  href="https://yamazato.nuee.nagoya-u.ac.jp/en/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title="Nagoya University Yamazato Laboratory"
-                >
+                <a className="navbar-item" href="https://yamazato.nuee.nagoya-u.ac.jp/en/" target="_blank" rel="noopener noreferrer" title="Nagoya University Yamazato Laboratory">
                   <FaWifi />
                   &nbsp;&nbsp;Yamazato Laboratory
                 </a>
@@ -105,8 +83,8 @@ const Footer = class extends React.Component {
           </div>
         </div>
       </footer>
-    )
+    );
   }
-}
+};
 
-export default Footer
+export default Footer;
